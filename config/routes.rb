af7dc55a698a4eea33556d94b9e46dbae71db3ce
tatsuma0928 <tatsuma.sano@icloud.com>
2020-08-02 16:36:26 +0900

    setting-events-view No.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'events#index'
+  resources :events
+  resources :users, only: [:index, :edit, :update]
 end
